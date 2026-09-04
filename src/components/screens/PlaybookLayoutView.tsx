fa4b256259f911f6a2a70c9f9ea4b1e11cd76d69
Tosun0 <mjo2469@naver.com>
2026-09-04 17:00:34 +0900

    20260904 UI: Render district thumbnails as sprites
    지역구 확대 상태에서도 썸네일이 항상 보이도록 카메라 정면 스프라이트로 교체했습니다.

--- src/components/screens/PlaybookLayoutView.tsx
+++ src/components/screens/PlaybookLayoutView.tsx
@@ -1110,10 +1110,9 @@ function PlaybookObject({
           <mesh geometry={mapMarkerGeometry} castShadow={shadowsEnabled} receiveShadow={shadowsEnabled}>
             <meshStandardMaterial {...material()} color={surfaceColor} roughness={0.2} metalness={0.22} />
           </mesh>
-          <mesh position={[0, 0, 0.22]} renderOrder={30}>
-            <planeGeometry args={[1.82, 0.86]} />
-            <meshBasicMaterial map={texture} transparent opacity={viewOpacity} toneMapped={false} depthTest={false} depthWrite={false} side={THREE.DoubleSide} />
-          </mesh>
+          <sprite position={[0, 0, 0.22]} scale={[1.82, 0.86, 1]} renderOrder={30}>
+            <spriteMaterial map={texture} transparent opacity={viewOpacity} toneMapped={false} depthTest={false} depthWrite={false} />
+          </sprite>
         </>
       );
     }
